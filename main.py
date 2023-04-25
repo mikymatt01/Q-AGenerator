d0f8a2f8ac8df4cc7db769bc9c1a5c94980c6494
Michele Mattiello <michele.mattiello2001@gmail.com>
2023-04-25 19:01:06 +0200

    fix

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from app import run
 import streamlit as st
 
 if __name__ == '__main__':
+    print('check 1')
     run("My name is Michele but my friends call me mikymatt")
+    print('check 2')
     paragraph = st.text_input("paragraph")
+    print('check 3')
     questgen = run(paragraph)
     st.json(questgen)
 #    demo = gr.Interface(fn=run, inputs="text", outputs="json")
