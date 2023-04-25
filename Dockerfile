880de517e041dc50a6b5d286e2b01891a15a4eb8
Michele Mattiello <michele.mattiello2001@gmail.com>
2023-04-25 18:22:31 +0200

    fix

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.9
 WORKDIR /code
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-EXPOSE 5000
+EXPOSE 8501
 COPY . .
 CMD python3 -m streamlit run main.py
+# -m streamlit run
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from app import run
 import streamlit as st
 
-paragraph = st.text_input("paragraph")
-questgen = run(paragraph)
-st.json(questgen)
-
+if __name__ == '__main__':
+    run("My name is Michele but my friends call me mikymatt")
+    paragraph = st.text_input("paragraph")
+    questgen = run(paragraph)
+    st.json(questgen)
+#    demo = gr.Interface(fn=run, inputs="text", outputs="json")
+#    demo.launch()
